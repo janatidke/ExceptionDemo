9f1f778b9f12b9984b221980455b9efe80f60412
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-09-05 18:03:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace ExceptionDemo
+{
+    class Exception
+    {
+        public void CalculateSum()
+        {
+            int i=0;
+            int[] num = new int[5] {12,13,14,15,15};
+            int sum = 0;
+            try
+            {
+                for (i = 0; i < 5; i++)
+                {
+                    sum += num[i];
+                }
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Please check the iteration of the list");
+            }
+            finally
+            {
+                Console.WriteLine("Total {0}", sum);
+            
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Handling Systems Exception:");
+            Exception index = new Exception();
+            index.CalculateSum();
+        }
+    }
+}
